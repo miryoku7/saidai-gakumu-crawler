7902a29b6567e05e59cd2ae80c36dba166b0abe7
miryokupoint-7 <84623421+miryokupoint-7@users.noreply.github.com>
2022-04-13 14:08:28 +0900

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//=========================  CONFIG  =========================//
+// Config USER_ID and PASSWORD
+
+define("USER_ID",     "your id");
+define("PASSWORD",    "your password");
+
+// example 
+// define("USER_ID",     "s21td023");
+// define("PASSWORD",    "DTc32du9c");
